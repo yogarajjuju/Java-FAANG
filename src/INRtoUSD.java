e61bb8dbddebfef9f099dfc17e14a5116846fdd1
Yogaraj <yogarajjuju22@gmail.com>
2025-01-24 16:01:33 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/INRtoUSD.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class IRNtoUSD {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double IRN = in.nextDouble();
+        double conversionrate = 75;
+        double USD = IRN
+    }
+}
