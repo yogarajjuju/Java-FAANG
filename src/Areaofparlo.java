e467934df5d1eff0a173783fb4998927b1b35df7
Yogaraj <yogarajjuju22@gmail.com>
2025-01-25 15:12:27 +0530

    Bankaiii

---
+++ src/Areaofparlo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Areaofparlo {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        double b = in.nextDouble();
+        double h = in.nextDouble();
+        double area = b * h;
+        System.out.println(area);
+    }
+}
---
+++ src/Areaoftri.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Areaoftri {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double base = in.nextDouble();
+        double height = in.nextDouble();
+         double area = 0.5 *  base * height;
+        System.out.println(area);
+    }
+}
