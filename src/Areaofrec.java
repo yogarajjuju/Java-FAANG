bdacff5a0d2f73c0fcf969b50f66aaf08bff50af
Yogaraj <yogarajjuju22@gmail.com>
2025-01-25 15:03:11 +0530

    Bankai

---
+++ src/Areaofrec.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Areaofrec {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double r = in.nextInt();
+        double area = 3.14 * r * r;
+        System.out.println(area);
+
+    }
+
+
+}
