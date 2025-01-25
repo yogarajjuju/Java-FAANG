9e0e625d852e191d793d85695540a2a017848233
Yogaraj <yogarajjuju22@gmail.com>
2025-01-25 15:22:37 +0530

    Bankaiii

---
+++ src/Areaofromb.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Areaofromb {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double d1 = in.nextDouble();
+        double d2 = in.nextDouble();
+        double area = 0.5 * d1 * d2;
+        System.out.println(area);
+    }
+}
