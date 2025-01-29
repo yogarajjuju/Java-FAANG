333045be8e900eb4921aa4fa05ffa91ffe914c62
Yogaraj <yogarajjuju22@gmail.com>
2025-01-29 11:23:55 +0530

    Bankai

--- src/HCF.java
+++ src/HCF.java
@@ -7,14 +7,22 @@ public class HCF {
         int num1 = in.nextInt();
         System.out.println("Enter the number 2");
         int num2 = in.nextInt();
+        int hcf = 0;
 //        int small = Math.min(num1,num2);
 //        (num1 < num2 ? num1 : num2) without inbuilt function
         for (int i = (num1 < num2 ? num1 : num2); i > 0; i--){
             if (num1 % i ==0 && num2 % i == 0){
+                hcf = i;
                 System.out.println("HCF is  " + i);
                 break;
+
             }
+
+
         }
+        int LCM = (num1 * num2) / hcf;
+        System.out.println( " The LCM is :" + LCM);
+
 
 
     }
